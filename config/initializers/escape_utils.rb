eb48cbbc1ae32829f6b256a13f0eb834bd7461bf
Maxim Kulkin <maxim.kulkin@gmail.com>
2012-04-26 09:44:43 +0400

    Fixed escape utils initialization

---
+++ config/initializers/escape_utils.rb
@@ -0,0 +1,11 @@
+require 'escape_utils/html/rack'
+require 'escape_utils/html/erb'
+require 'escape_utils/html/cgi'
+
+require 'escape_utils/url/cgi'
+require 'escape_utils/url/rack'
+require 'escape_utils/url/erb'
+require 'escape_utils/url/uri'
+
+require 'escape_utils/javascript/action_view'
+
