b5012012a1108fca5aea47cc2be3ff58b7cea99e
Maxim Kulkin <maxim.kulkin@gmail.com>
2012-05-07 20:35:08 +0400

    Fixed product permalink generation

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,4 +1,8 @@
 Spree::Product.class_eval do
   reflect_on_association(:product_properties).options[:order] = 'position asc'
+
+  def to_param
+    permalink.present? ? permalink : (permalink_was || name.to_s.to_url.gsub("'", ''))
+  end
 end
 
