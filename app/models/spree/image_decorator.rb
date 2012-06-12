f431fa0daab6bf71c1f5b1548d7f174619274c3e
Maxim Kulkin <maxim.kulkin@gmail.com>
2012-06-12 15:54:45 +0400

    Updated product image sizes

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -1,5 +1,17 @@
 Spree::Image.class_eval do
-  attachment_definitions[:attachment][:convert_options] ||= {}
-  attachment_definitions[:attachment][:convert_options][:product] = "-background white -gravity center -extent 240x240"
+  attachment_definitions[:attachment][:styles] = {
+    :mini => '65x65>',
+    :small => '160x160>',
+    :product => '300x300>',
+    :large => '600x600>'
+  }
+
+  attachment_definitions[:attachment][:convert_options] = {
+    :all => "-background white -gravity center",
+    :mini => "-extent 65x65",
+    :small => "-extent 160x160",
+    :product => "-extent 300x300",
+    :large => '-extent 600x600'
+  }
 end
 
