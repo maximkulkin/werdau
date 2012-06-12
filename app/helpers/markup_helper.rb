a45eb93f13a7aa72654fe34c28b2c0ece98c0b79
Maxim Kulkin <maxim.kulkin@gmail.com>
2012-06-13 02:38:44 +0400

    Made working 'purchase' button

--- app/helpers/markup_helper.rb
+++ app/helpers/markup_helper.rb
@@ -1,6 +1,14 @@
+# encoding: utf-8
 module MarkupHelper
   def section_title(title, options={})
     options[:class] = "#{options[:class]} section"
     content_tag(:h2, content_tag(:span, content_tag(:span, title)), options)
+  end
+
+  def purchase_button_for(product)
+    form_for(:order, :url => populate_orders_url, :html => { :class => 'buy' }) do |f|
+      concat hidden_field_tag("variants[#{product.master.id}]", 1)
+      concat submit_tag('Купить')
+    end
   end
 end
