5740c46a3ecff16be1ca7a634079d32e06035c49
Maxim Kulkin <maxim.kulkin@gmail.com>
2012-06-18 04:56:16 +0400

    Enabled TinyMCE asset precompilation; fixed TinyMCE frontend styles

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,7 @@ module Werdau
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.precompile += ['tinymce_werdau.css']
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
