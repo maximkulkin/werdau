8e58d3f3b6e79787973b59c340f3f15cfa58516a
Maxim Kulkin <maxim.kulkin@gmail.com>
2012-07-01 23:45:26 +0400

    Added helper for bpanel

---
+++ app/helpers/bpanel_helper.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+module BpanelHelper
+  def bpanel_tab(title, type, url='#')
+    link_to raw("<span class='lc'></span><span class='ic'>#{title}</span><span class='rc'></span>"), url, :class => "tab #{type}"
+  end
+
+  def bpanel_account_tab
+    bpanel_tab "Личный кабинет", :account, account_path
+  end
+
+  def bpanel_compare_tab
+    bpanel_tab "К сравнению (0)", :compare
+  end
+
+  def bpanel_bookmarks_tab
+    bpanel_tab "Закладки (0)", :bookmarks
+  end
+
+  def bpanel_viewed_recently_tab
+    bpanel_tab "Просмотренные (0)", :viewed_recently
+  end
+
+  def bpanel_cart_tab
+    bpanel_tab "Корзина (0)", :cart
+  end
+end
+
