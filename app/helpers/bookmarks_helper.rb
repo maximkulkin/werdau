339990e530fafdf76916f82e94136c9c50511ec7
Maxim Kulkin <maxim.kulkin@gmail.com>
2012-07-08 04:08:27 +0400

    Fixed bookmark_link helper not playing well if user is not authenticated

--- app/helpers/bookmarks_helper.rb
+++ app/helpers/bookmarks_helper.rb
@@ -2,8 +2,10 @@ module BookmarksHelper
   def bookmark_link(variant)
     options = { :class => "bookmark variant-#{variant.id}" }
 
-    return link_to(t(:in_bookmarks), bookmarks_path, options) \
-      if current_user.bookmarked_variants.include? variant
+    if current_user
+      return link_to(t(:in_bookmarks), bookmarks_path, options) \
+        if current_user.bookmarked_variants.include? variant
+    end
 
     link_to t(:bookmark), bookmark_path(:id => variant),
       options.merge(:method => :post, :remote => true)
