59f62a0858ea29ddcc2f901782987529a7ee6405
Maxim Kulkin <maxim.kulkin@gmail.com>
2012-07-23 03:19:36 +0400

    Made recently viewed products auto close on click outside of it

--- app/assets/javascripts/store/recently_viewed_products.js
+++ app/assets/javascripts/store/recently_viewed_products.js
@@ -3,4 +3,9 @@ $(document).ready(function() {
     event.preventDefault()
     $('#recently_viewed_products').toggle()
   })
+  $('body').on('click', function(event) {
+    if($(event.target).closest('.bpanel').length == 0) {
+      $('#recently_viewed_products').hide()
+    }
+  })
 })
