24f83247524f7ccf3023ed211dfe8d8426a240a4
Maxim Kulkin <maxim.kulkin@gmail.com>
2012-07-24 04:26:36 +0400

    Fixed order total update when adding new product

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -1,3 +1,15 @@
 Spree::OrdersController.class_eval do
   respond_to :js, :only => [:edit, :update, :populate]
+
+  def populate
+    @order = current_order(true)
+    params[:variants].each do |variant_id, quantity|
+      quantity = quantity.to_i
+      @order.add_variant(Spree::Variant.find(variant_id), quantity) if quantity > 0
+    end if params[:variants]
+
+    @order.update!
+
+    respond_with(@order, :location => cart_path)
+  end
 end
