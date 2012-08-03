50836cf5ed25440fed8cf89f12d63cc758ed792b
Maxim Kulkin <maxim.kulkin@gmail.com>
2012-08-03 04:38:32 +0400

    Fixed unicorn stop action; added conditional precompile speedup

--- config/deploy.rb
+++ config/deploy.rb
@@ -154,7 +154,19 @@ namespace :deploy do
 
   desc "Stop unicorn server"
   task :stop, :roles => :app, :except => { :no_release => true } do
-    run "kill -TERM `cat #{unicorn_pid}`"
+    run "[ -e #{unicorn_pid} -a -e /proc/`cat #{unicorn_pid}` ] && kill -TERM `cat #{unicorn_pid}`; true"
   end
 end
-
+ 
+namespace :deploy do
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
+end
