0ef70260eb285233f093af0e599f892252abda6f
Maxim Kulkin <maxim.kulkin@gmail.com>
2012-09-14 01:13:45 +0400

    Added name of imported file into product import message

--- app/controllers/spree/admin/product_import_controller.rb
+++ app/controllers/spree/admin/product_import_controller.rb
@@ -4,12 +4,13 @@ class Spree::Admin::ProductImportController < Spree::Admin::BaseController
   end
   
   def import
+    filename = params[:data].original_filename
     Werdau::CsvProductImporter.import(params[:data].tempfile)
 
-    flash[:notice] = I18n.t('product_import_result.success')
+    flash[:notice] = I18n.t('product_import_result.success', :filename => filename)
     redirect_to admin_products_path
   rescue Exception => e
-    @error_message = I18n.t('product_import_result.failure', :message => e.to_s)
+    @error_message = I18n.t('product_import_result.failure', :filename => filename, :message => e.to_s)
     render 'form'
   end
 end
